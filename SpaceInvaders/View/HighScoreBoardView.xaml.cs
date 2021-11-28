f3d877037706be77b71de7668a469adfedb74509
Kenneth Mitchell <kmitche9@my.westga.edu>
2021-11-28 15:02:00 -0500

    I created a basic UI for the High Score Board. I also added the CodeBehind and ViewModel classes, but they still need to be implemented.

---
+++ SpaceInvaders/View/HighScoreBoardView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.View
+{
+    public sealed partial class HighScoreBoardView
+    {
+
+        #region Constructors
+
+        public HighScoreBoardView()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void sortByNameFirst(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void sortByLevelFirst(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
